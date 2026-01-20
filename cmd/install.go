4dd6a55bfc8987718665dcf4b4f7230bd3a6e916
kaio-dot <kaiolyan22@gmail.com>
2026-01-20 14:43:54 -0300

    Subindo comando install na paleta de comandos

---
+++ cmd/install.go
@@ -0,0 +1,23 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var installCmd = &cobra.Command{
+	Use:   "install",
+	Short: "Instala a ferramenta em ambiente local",
+	Args:  cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+
+		tool := args[0]
+		fmt.Println("Iniciando a instalação")
+		fmt.Println("Instalando:", tool)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(installCmd)
+}
